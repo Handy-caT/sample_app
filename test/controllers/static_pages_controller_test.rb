027e65d76e7e91ed31f2281d19247ba5361418ab
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2022-02-19 10:28:55 +0300

    Tests added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,23 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Sample App"
+    assert_select "title", full_title("")
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help | Sample App"
+    assert_select "title", full_title("Help")
   end
 
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select "title", "About | Sample App"
+    assert_select "title", full_title("About")
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,12 +5,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid registration information" do
     get sign_up_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name: "",
-                               email: "user@invalid",
-                               password: "foo",
-                               password_confirmation: "bar"}
+      post users_path, params:{ user: {name: "",
+                                       email: "user@invalid",
+                                       password: "foo",
+                                       password_confirmation: "bar"} }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+  end
+
+  test "valid registration information" do
+    get sign_up_path
+    assert_difference 'User.count' do
+      post users_path, params:{ user: {name: "John",
+                                       email: "user@valid.com",
+                                       password: "RealPassword",
+                                       password_confirmation: "RealPassword"} }
+      follow_redirect!
+    end
+    assert_template 'users/show'
+    assert_not flash.blank?
   end
 
 end
