faeb258c72aff454925e67ec287411d7aa524c15
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2022-02-19 17:18:07 +0300

    Sessions and log out added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def new
+
   end
 
   def create
 
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-
+      log_in user
+      redirect_to user
+      flash[:success] = 'Welcome back, ' + user.name
     else
       if !user
         flash.now[:danger] = 'User with that email not found! Try again'
@@ -17,7 +20,11 @@ class SessionsController < ApplicationController
       render 'new'
     end
 
+  end
 
+  def destroy
+    log_out
+    redirect_to root_url
   end
 
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,6 +2,10 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = users(:john)
+  end
+
   test "login with invalid information" do
     get log_in_path
     assert_template 'sessions/new'
@@ -12,5 +16,27 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
+  test "login with valid information" do
+    get log_in_path
+    post log_in_path, params: { session: { email: @user.email, password: "password"} }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_not flash.empty?
+
+    assert_template 'users/show'
+    assert_select "a[href=?]", log_in_path, count: 0
+    assert_select "a[href=?]", log_out_path
+    assert_select "a[href=?]", user_path(@user)
+
+    delete log_out_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+
+    assert_select "a[href=?]", log_in_path
+    assert_select "a[href=?]", log_out_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+  end
 
 end
